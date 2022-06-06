1facfa2ca5dc007d1aba9523518aa4429fd2e33b
Gaurab Rayamajhi <106250922+Gaurab2000@users.noreply.github.com>
2022-06-06 19:42:25 +0545

    Update ai.py

--- AI Desktop Voice Assistant/ai.py
+++ AI Desktop Voice Assistant/ai.py
@@ -18,6 +18,7 @@ import os
 import smtplib
 import time
 
+
 flags=QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
 engine=pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -36,60 +37,71 @@ def wishme():
             speak("Good Evening Gaurrab!")
         
         speak("Jarvis at your service, how may i help you ?")
-def takeCommand():
+class mainT(QThread):
+    def __init__(self):
+        super(mainT,self).__init__()
+
+    def run(self):
+        self.JARVIS()
+    def STT(self):
  #it takes microphone input from the user and return string output
-    r= sr.Recognizer()
-    with sr.Microphone()as source:
+     r= sr.Recognizer()
+     with sr.Microphone()as source:
         print("I am listening")
         r.pause_threshold = 1
         audio = r.listen(source)
-    try:
+     try:
          print("Recognizing...")    
          query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
          print(f"User said: {query}\n")  #User query will be printed.
-    except Exception as e:
+     except Exception as e:
         # print(e)    
         print("Say that again please...")   #Say that again will be printed in case of improper voice 
         return "None" #None string will be returned
-    return query
-def sendEmail(to,content):
-    server = smtplib.SMTP('smtp.gmail.com',587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com','your-password')
-    server.sednmail('youremail@gmail.com',to,content)
-    server.close
+     query=query.lower()
+     return query
+    def sendEmail(to,content):
+     server = smtplib.SMTP('smtp.gmail.com',587)
+     server.ehlo()
+     server.starttls()
+     server.login('youremail@gmail.com','your-password')
+     server.sednmail('youremail@gmail.com',to,content)
+     server.close
 
-if __name__=="__main__":
-  wishme()
-  while True:
+    def JARVIS(self):
+       wishme()
+       while True:
+      
     
-       query=takeCommand().lower()
+        self.query=self.STT().lower()
 
-       if 'wikipedia' in query:
+        if 'wikipedia' in query:
            speak('searching wikipedia.....')
            query = query.replace("wikipedia","")
            result = wikipedia.summary(query, sentences=2)
            speak("Accoding to Wikipedia")
            speak(result)
-       elif 'open youtube' in query:
+        elif 'open youtube' in query:
            webbrowser.open("youtube.com")
-       elif 'open google' in query:
+        elif 'open google' in query:
            webbrowser.open("google.com")
-       elif 'open stackoverflow' in query:
+        elif 'open stackoverflow' in query:
            webbrowser.open("stackoverflow.com")
-       elif'play music' in query:
+        elif'play music' in query:
            music_dir = 'D:\\New folder\\Audio'
            songs= os.listdir(music_dir)
            print(songs)
            os.startfile(os.path.join(music_dir,songs[0]))
-       elif 'the time' in query:
+        elif 'the time' in query:
             strTime= datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir, the time is {strTime}") 
-       elif 'open code' in query:
+        elif 'open code' in query:
            codePath="D:\\Microsoft VS Code\\Code.exe"
            os.startfile(codePath)
-       elif 'email to harry' in query:
+        elif 'turn off'in query :
+           break  
+
+       """elif 'email to harry' in query:
            try:
                speak("what should i say?")
                content= takeCommand()
@@ -98,32 +110,30 @@ if __name__=="__main__":
                speak("Email has been sent!")
            except Exception as e:
                print(e)
-               speak("sorry, i am not able to send email")
-       elif 'turn off'in query :
-           break  
-
+               speak("sorry, i am not able to send email")"""
+     
 
 
 FROM_MAIN,_ = loadUiType(os.path.join(os.path.dirname(__file__),"./scifi.ui"))
 class Main(QMainWindow,FROM_MAIN):
     def __init__(self, parent=None): 
-        super().__init__(parent)
+        super(Main,self).__init__(parent)
         self.setupUi(self)
         self.setFixedSize(1920,1080)
         self.label_7=QLabel
-        self.exitB.setStyleSheet("background-image:url(.lib/exit - Copy.png);\n"
+        self.exitB.setStyleSheet("background-image:url(./exit - Copy.png);\n"
         "border.none;")
         self.exitB.clicked.connect(self.close)
         self.setWindowFlag(flags)
         Dspeak = mainT()
-        self.label_7=Qmovie("./lib/gifloader.gif",QByteArray(),self)
+        self.label_7=Qmovie("./gifloader.gif",QByteArray(),self)
         self.label_7.setCacheMode(QMovie.CacheAll)
-        self.label_4.setMovie(self.label_7)
+        self.label_6.setMovie(self.label_7)
         self.label_7.start()
 
         self.ts =time.strftime("%A,%d %B")
         Dspeak.start()
-        self.label.setPixmap(QPixmap("./lib/bg.png"))
+        self.label.setPixmap(QPixmap("./bg.png"))
         self.label_5.setText("<font size=8 color='white'>"+self.ts+"</font>")
         self.label_5.setFont(Qfont(Qfont('Acens',8)))
 
