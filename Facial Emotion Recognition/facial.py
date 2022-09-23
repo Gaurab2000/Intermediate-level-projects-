9fd52c8e4a4c46e91aabc2d0cde9ae63f8e410d6
Gaurab Rayamajhi <106250922+Gaurab2000@users.noreply.github.com>
2022-09-23 09:21:23 +0545

    v2

--- Facial Emotion Recognition/facial.py
+++ Facial Emotion Recognition/facial.py
@@ -1,34 +1,51 @@
 import os
 import cv2
 import numpy as np
-
-FaceCascade=cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-
-cap=cv2.VideoCapture(0)
-if not (cap.isOpened()):
-    print("cam could not be opened")
-
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,300)
+from keras.models import  load_model
+from keras.preprocessing import image
+#from keras.preprocessing.image import  load_img, img_to_array 
+from tensorflow.keras.utils import img_to_array
+from time import sleep
+# load model
+model = load_model("best_model.h5")
 
 
-while(True):
-        ret,frame=cap.read()
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture(0)
 
-        faces = FaceCascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
+while True:
+    ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
+    if not ret:
+        continue
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
 
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
-        for (x, y, w, h) in faces:
+    for (x, y, w, h) in faces_detected:
+        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+        roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
+        roi_gray = cv2.resize(roi_gray, (224, 224))
+        img_pixels = img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255
 
-           
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)  #BGR, 
+        predictions = model.predict(img_pixels)
 
-        cv2.imshow('frames',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # find max indexed array
+        max_index = np.argmax(predictions[0])
 
-cv2.destroyAllWindows()
+        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+        predicted_emotion = emotions[max_index]
+
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    resized_img = cv2.resize(test_img, (1000, 700))
+    cv2.imshow('Facial emotion analysis ', resized_img)
+
+    if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+        break
+
 cap.release()
+cv2.destroyAllWindows
